919cb1a018295f9c2faaff6f1457d445053c5022
Ron Smits <ron.smits@gmail.com>
2016-10-20 22:15:49 +0200

    updated kotlin and tornadofx version made all code work again by adding the main method

--- login/src/main/java/no/tornado/fxsample/login/LoginApp.kt
+++ login/src/main/java/no/tornado/fxsample/login/LoginApp.kt
@@ -1,11 +1,11 @@
 package no.tornado.fxsample.login
 
+import javafx.application.Application
 import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.importStylesheet
 
-class LoginApp : App() {
-    override val primaryView = LoginScreen::class
+class LoginApp : App(LoginScreen::class) {
     val loginController: LoginController by inject()
 
     override fun start(stage: Stage) {
@@ -13,4 +13,8 @@ class LoginApp : App() {
         super.start(stage)
         loginController.init()
     }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(LoginApp::class.java, *args)
 }
