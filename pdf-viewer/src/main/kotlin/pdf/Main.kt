e63eccaa1c679e93aa14e30b9a89b762c100b420
Miguel Carboni <mcarboni@siu.edu.ar>
2017-09-19 10:41:26 -0300

    pdf viewer example

--- pdf-viewer/src/main/kotlin/pdf/Main.kt
+++ pdf-viewer/src/main/kotlin/pdf/Main.kt
@@ -1,8 +1,5 @@
-import com.sun.deploy.util.BlackList
 import javafx.application.Application
-import javafx.beans.property.SimpleFloatProperty
-import javafx.beans.property.SimpleIntegerProperty
-import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.*
 import javafx.embed.swing.SwingFXUtils
 import javafx.geometry.Pos
 import javafx.scene.effect.BlurType
@@ -10,100 +7,168 @@ import javafx.scene.effect.InnerShadow
 import javafx.scene.image.Image
 import javafx.scene.image.WritableImage
 import javafx.scene.paint.Color
-import javafx.scene.text.TextAlignment
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.rendering.PDFRenderer
 import tornadofx.*
-import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.net.URI
 
 class PdfViewerApp : App(PdfViewer::class) {
 
-    companion object {
 
+    companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             Application.launch(PdfViewerApp::class.java, *args)
+
+
         }
 
     }
 
+    override fun onBeforeShow(view: UIComponent) {
+        val uri = PdfViewer::class.java.getResource("/pdf/tornadofx-guide.pdf").toURI().toString()
+        (view as PdfViewer).uriString(uri)
+    }
+
+}
+class PdfViewModel: ViewModel() {
+    var documentURIString = SimpleStringProperty("")
+    var documentURI = SimpleObjectProperty<URI>(URI(""))
+    var documentInputStream = SimpleObjectProperty<InputStream>()
+    var document: PDDocument? = null
+    var pdfRenderer : PDFRenderer? = null
+    val currentPage = SimpleObjectProperty<Image>(WritableImage(1,1))
+    val currentPageNumber = SimpleIntegerProperty(0)
+    val pageCount = SimpleIntegerProperty(0)
+    val scale = SimpleFloatProperty(1f)
+
+
+    init {
+        documentInputStream.onChange { input ->
+            if (input is InputStream) {
+                document = PDDocument.load(input)
+                pdfRenderer = PDFRenderer(document)
+                pageCount.value = document?.pages?.count
+                openPage(0)
+            }
+        }
+        documentURIString.onChange { documentURI.value = URI(it) }
+        documentURI.onChange { nuevaUri ->
+            val input = when (nuevaUri!!.scheme) {
+                "file" -> FileInputStream(nuevaUri.toURL().file)
+                "http", "https" -> {
+                    val client = HttpClientBuilder.create().build()
+                    val request = HttpGet(nuevaUri)
+                    val response = client.execute(request)
+                    response.entity.content
+                }
+                else -> null
+            }
+            documentInputStream.value = input
+        }
+        currentPageNumber.onChange {
+            n ->
+            openPage(n)
+            log.info("Cambio a pagina ${n}")
+
+        }
+
+
+    }
+
+    fun openPage(pageCounter: Int) {
+        val bim = pdfRenderer?.renderImage(pageCounter, scale.value)//pdfRenderer?.renderImageWithDPI(pageCounter, 300)
+        if (bim != null) {
+            currentPage.value = SwingFXUtils.toFXImage(bim, null)
+        }
+    }
+
+    fun firstPage() {
+        currentPageNumber.value = 0
+    }
+
+    fun previousPage() {
+        --currentPageNumber.value
+    }
+
+    fun nextPage() {
+        ++currentPageNumber.value
+    }
+
+    fun lastPage() {
+        currentPageNumber.value = pageCount.value - 1
+    }
+
+    var isFirst = currentPageNumber.isEqualTo(0)
+
+    var isLast = currentPageNumber.isEqualTo(pageCount - 1)
+
 }
 
-class PdfViewer: View("Pdf Viewer") {
-    val documentProperty = SimpleObjectProperty<PDDocument>()
-    var document by documentProperty
-    val currentPageProperty = SimpleObjectProperty<Image>(WritableImage(1,1))
-    var currentPage by currentPageProperty
-    val currentPageNumberProperty = SimpleIntegerProperty(0)
-    var currentPageNumber by currentPageNumberProperty
-    val pageCountProperty = SimpleIntegerProperty(0)
-    var pageCount by pageCountProperty
-    val scaleProperty = SimpleFloatProperty(1f)
-    var scale by scaleProperty
-    var pdfRenderer : PDFRenderer? = null
-
+class PdfViewer: Fragment("Pdf Viewer") {
+    val pdfModel: PdfViewModel by inject()
 
     override val root = borderpane {
         top = hbox {
+
             style {
                 alignment = Pos.CENTER
                 padding = box(20.px)
             }
                 button("|<") {
                     action {
-                        currentPageNumberProperty.value = 0
+                        pdfModel.firstPage()
+                    }
+                    disableWhen {
+                        pdfModel.isFirst
                     }
                 }
                 button("<") {
                     action {
-                        currentPageNumberProperty.value--
+                        pdfModel.previousPage()
+                    }
+                    disableWhen {
+                        pdfModel.isFirst
                     }
                 }
-                textfield (currentPageNumberProperty + 1)
-                label(pageCountProperty)
+                textfield (pdfModel.currentPageNumber + 1)
+                label(pdfModel.pageCount)
                 button(">") {
                     action {
-                        currentPageNumberProperty.value++
+                        pdfModel.nextPage()
+                    }
+                    disableWhen {
+                        pdfModel.isLast
                     }
                 }
                 button(">|"){
                     action {
-                        currentPageNumberProperty.value = Math.max(pageCountProperty.value - 1, 0)
+                        pdfModel.lastPage()
+                    }
+                    disableWhen {
+                        pdfModel.isLast
                     }
                 }
+
         }
         center = scrollpane {
             style {
                 padding = box(0.px, 60.px, 0.px, 60.px)
                 backgroundColor += Color.DARKGRAY
                 effect = InnerShadow(BlurType.THREE_PASS_BOX, Color.GRAY, 10.0, 10.0,10.0,10.0 )
-
             }
             vbox {
-                imageview {
-                    currentPageProperty.onChange {
-                        this.image = currentPage
-                        this.autosize()
-                    }
-                }
+                imageview(pdfModel.currentPage)
             }
         }
     }
 
-    init {
-        document = PDDocument.load(File("/home/mcarboni/digitalsignatures20130304.pdf"))
-        pdfRenderer = PDFRenderer(document)
-        pageCountProperty.value = document?.pages?.count
-
-        currentPageNumberProperty.onChange { n -> openPage(n) }
-        openPage(0)
+    fun uriString(uri: String) {
+        pdfModel.documentURIString.value = uri
     }
 
-    private fun openPage(pageCounter: Int) {
-        val bim = pdfRenderer?.renderImage(pageCounter, scale)//pdfRenderer?.renderImageWithDPI(pageCounter, 300)
-        if (bim != null) {
-            val writableImage = WritableImage(bim.width, bim.height)
-            currentPage = SwingFXUtils.toFXImage(bim, null)
-        }
-    }
 }
