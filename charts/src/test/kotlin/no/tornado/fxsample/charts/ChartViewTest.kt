a4c92a29148cdbf6013fbdb0d81c4e65662cbce1
chrisn <maflaja@gmail.com>
2021-10-14 10:40:23 +1100

    Implemented test for charts project.

--- charts/src/test/kotlin/no/tornado/fxsample/charts/ChartViewTest.kt
+++ charts/src/test/kotlin/no/tornado/fxsample/charts/ChartViewTest.kt
@@ -1,6 +1,8 @@
 package no.tornado.fxsample.charts
 
 import no.tornado.fxsample.TestBase
+import org.testfx.assertions.api.Assertions.assertThat
+import tornadofx.*
 import kotlin.test.Test
 
 class ChartViewTest: TestBase() {
@@ -10,6 +12,11 @@ class ChartViewTest: TestBase() {
 
     @Test
     fun testMainView() {
+        assertThat(testedView.root).hasExactlyChildren(8, ".chart")
 
+        testedView.root.getChildList()?.forEach {
+            assertThat(it).isVisible
+            assertThat(it).hasStyleableParent(testedView.root)
+        }
     }
 }
