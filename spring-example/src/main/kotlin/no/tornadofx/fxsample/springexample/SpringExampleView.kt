442feb63d1e11956ca87fffcc25b59534f1a29c6
chrisn <maflaja@gmail.com>
2021-10-30 14:28:09 +1100

    Implemented test for spring-example project.

--- spring-example/src/main/kotlin/no/tornadofx/fxsample/springexample/SpringExampleView.kt
+++ spring-example/src/main/kotlin/no/tornadofx/fxsample/springexample/SpringExampleView.kt
@@ -3,7 +3,7 @@ package no.tornadofx.fxsample.springexample
 import tornadofx.*
 
 class SpringExampleView : View() {
-    val bean : HelloBean by di()
+    private val bean : HelloBean by di()
     override val root = vbox {
         label(bean.helloworld()).paddingAll=20
     }
