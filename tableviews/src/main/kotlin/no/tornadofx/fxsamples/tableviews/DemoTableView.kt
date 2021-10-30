7cd99316548f5c0e3fde6223ac7d6c612183e686
chrisn <maflaja@gmail.com>
2021-10-30 15:12:40 +1100

    Implemented test for tableviews project.

--- tableviews/src/main/kotlin/no/tornadofx/fxsamples/tableviews/DemoTableView.kt
+++ tableviews/src/main/kotlin/no/tornadofx/fxsamples/tableviews/DemoTableView.kt
@@ -7,14 +7,13 @@ import tornadofx.*
 class DemoTableView : View() {
     override val root = GridPane()
 
-    val mapTableContent = mapOf(Pair("item 1", 5), Pair("item 2", 10), Pair("item 3", 6))
-
     init {
         with (root) {
             row {
                 vbox {
                     label("Tableview from a map")
                     tableview(FXCollections.observableArrayList<Map.Entry<String, Int>>(mapTableContent.entries)) {
+                        id = tableViewId
                         readonlyColumn("Item", Map.Entry<String, Int>::key)
                         readonlyColumn("Count", Map.Entry<String, Int>::value)
                         resizeColumnsToFitContent()
@@ -22,6 +21,11 @@ class DemoTableView : View() {
                 }
             }
         }
+    }
+
+    companion object {
+        val mapTableContent = mapOf(Pair("item 1", 5), Pair("item 2", 10), Pair("item 3", 6))
+        const val tableViewId = "mapTable"
     }
 
 }
