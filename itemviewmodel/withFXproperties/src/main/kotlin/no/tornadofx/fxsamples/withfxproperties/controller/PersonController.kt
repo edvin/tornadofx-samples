8c9a10606be4c211ae8f074bdf222531e9218c11
chrisn <maflaja@gmail.com>
2021-11-28 15:20:59 +1100

    Fixed the app functionality and added matching tests.

--- itemviewmodel/withFXproperties/src/main/kotlin/no/tornadofx/fxsamples/withfxproperties/controller/PersonController.kt
+++ itemviewmodel/withFXproperties/src/main/kotlin/no/tornadofx/fxsamples/withfxproperties/controller/PersonController.kt
@@ -1,26 +1,15 @@
 package no.tornadofx.fxsamples.withfxproperties.controller
 
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
 import no.tornadofx.fxsamples.withfxproperties.model.Person
 import no.tornadofx.fxsamples.withfxproperties.model.PersonModel
 import no.tornadofx.fxsamples.withfxproperties.model.PhoneNumber
-import tornadofx.Controller
+import tornadofx.*
 
 class PersonController : Controller() {
-    val persons: ObservableList<Person> = FXCollections.observableArrayList()
+    var persons = listOf(
+        Person(42, "John Doe", mutableListOf(PhoneNumber("47", "33349700"), PhoneNumber("47", "333943222"))),
+        Person(43, "Jane Doe", mutableListOf(PhoneNumber("1", "312 213 21")))
+    ).asObservable()
+
     val selectedPerson = PersonModel()
-
-    companion object {
-        val samplePersonList = listOf(
-            Person(42, "John Doe",listOf(PhoneNumber("47", "33349700"), PhoneNumber("47", "333943222"))),
-            Person(43, "Jane Doe", listOf(PhoneNumber("1", "312 213 21")))
-        )
-    }
-
-    init {
-        // Add some test persons for the demo
-        persons.add(samplePersonList[0])
-        persons.add(samplePersonList[1])
-    }
 }
